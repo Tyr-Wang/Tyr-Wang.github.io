c7f48374cc0897848a47b6244e1062f2cfc6bd24
GitHub Tyr-Wang <GitHub yysxwzj@126.com>
2019-08-30 14:18:28 +0800

    Site updated: 2019-08-30 14:18:28

---
+++ uploads/md2hexo.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+def main():
+    os.chdir(r"D:\Programs\Hexo-Blog\source\_posts")
+    fls = os.listdir(r".")
+    aim_ls = []
+    for i in fls:
+        if i[-3:] == ".md":
+            aim_ls.append(i)
+
+    for i in aim_ls:
+        content = ""
+        with open(i, "r", encoding='UTF-8') as f:
+            content = f.read()
+
+        if content == '':
+            continue
+
+        try:
+            content = re.sub(r"(!\[.*?\]\()\\?/?(images)\\?/?(pasted-\d\d?\d?\d?\.png\))", "\g<1>/\g<2>/\g<3>", content)
+            f = open(i, "w", encoding='UTF-8')
+            f.write(content)
+            f.close()
+        except AttributeError as e:
+            print(e)
+
+        finally:
+            f.close()
+
+
+if __name__ == '__main__':
+    main()
